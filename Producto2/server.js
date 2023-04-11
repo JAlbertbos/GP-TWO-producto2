572296d7c376505438bf860d71263d06b8678cd4
KaidouoftheBeasts <dgomeztom@uoc.edu>
2023-04-11 22:29:39 +0200

    Conexión a MongoDB

--- Producto2/server.js
+++ Producto2/server.js
@@ -1,64 +1,85 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = express();
 
-const server = http.createServer((req, res) => {
-  console.log(`Petición ${req.method} ${req.url}`);
+// Conexión con MongoDB
+const MONGODB_URI = 'mongodb+srv://David:1234@agendasemanal.zbsfqm3.mongodb.net/AgendaSemanal';
 
-  // Obtener la ruta del archivo solicitado
-  let filePath = '.' + req.url;
-  if (filePath === './') {
-    filePath = 'Producto2/Dashboard.html';
-  }
-
-  // Obtener la extensión del archivo solicitado
-  const extname = path.extname(filePath);
-  let contentType = 'text/html';
-  switch (extname) {
-    case '.js':
-      contentType = 'text/javascript';
-      break;
-    case '.css':
-      contentType = 'text/css';
-      break;
-    case '.json':
-      contentType = 'application/json';
-      break;
-    case '.png':
-      contentType = 'image/png';
-      break;
-    case '.jpg':
-      contentType = 'image/jpg';
-      break;
-    case '.wav':
-      contentType = 'audio/wav';
-      break;
-  }
-
-  // Leer el archivo solicitado
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      if (err.code == 'ENOENT') {
-        // Archivo no encontrado
-        res.writeHead(404);
-        res.end('Archivo no encontrado');
-      } else {
-        // Otro error
-        res.writeHead(500);
-        res.end(`Error interno del servidor: ${err.code}`);
-      }
-    } else {
-      // Archivo encontrado
-      res.writeHead(200, { 'Content-Type': contentType });
-      res.end(content, 'utf-8');
-    }
-  });
-
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Servidor web iniciado en http://${hostname}:${port}/`);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => console.log('Connected to MongoDB'));
+
+// Definir el modelo de datos
+const tareaSchema = new mongoose.Schema({
+  titulo: String,
+  descripcion: String,
+  fecha: Date,
 });
+
+const Tarea = mongoose.model('Tarea', tareaSchema);
+
+// Configurar Express para servir archivos estáticos y estilos CSS
+app.use(express.static(path.join(__dirname)));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Rutas de la aplicación
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Dashboard.html'));
+});
+
+app.get('/api/tareas', async (req, res) => {
+  try {
+    const tareas = await Tarea.find();
+    res.json(tareas);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+});
+
+app.post('/api/tareas', async (req, res) => {
+  try {
+    const { titulo, descripcion, fecha } = req.body;
+    const tarea = new Tarea({ titulo, descripcion, fecha });
+    await tarea.save();
+    res.json(tarea);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+});
+
+app.put('/api/tareas/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { titulo, descripcion, fecha } = req.body;
+    const tarea = await Tarea.findByIdAndUpdate(id, { titulo, descripcion, fecha }, { new: true });
+    res.json(tarea);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+});
+
+app.delete('/api/tareas/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Tarea.findByIdAndDelete(id);
+    res.json({ message: 'Tarea eliminada correctamente' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+});
+
+// Iniciar el servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor web iniciado en el puerto ${PORT}`));
