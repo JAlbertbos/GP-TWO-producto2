27f704a2c87c8e1c9de661f875cf0bb4e2a6c33d
LauRoConUOC <lrodriguezconta@uoc.edu>
2023-04-17 08:50:40 +0200

    conflictos

--- Producto2/scripts/create-week.js
+++ Producto2/scripts/create-week.js
@@ -1,4 +1,6 @@
+
 document.addEventListener("DOMContentLoaded", () => {
+  
   const confirmBtn = document.getElementById("confirmButton");
   const cardForm = document.getElementById("cardForm");
 
@@ -16,53 +18,17 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     return await response.json();
   }
-
-  async function createCard(name, week, priority, year, description) {
-    const query = `
-      mutation CreateTask($title: String!, $description: String, $dueDate: String) {
-        createTask(title: $title, description: $description, dueDate: $dueDate) {
-          id
-        }
-      }
-    `;
-
-    const dueDate = new Date(year, 0, 1);
-    dueDate.setDate(dueDate.getDate() + (week - 1) * 7);
-
-    const variables = {
-      title: name,
-      description: description,
-      dueDate: dueDate.toISOString(),
-    };
-
-    try {
-      const result = await fetchGraphQL(query, variables);
-      if (result.errors) {
-        console.error(result.errors);
-        mostrarModal("Ocurrió un error al guardar la semana en el servidor.");
-        return;
-      }
-      // Aquí necesitas obtener el ID generado por GraphQL en lugar de generar uno aleatorio
-      const id = result.data.createTask.id;
-      // Llama a la función addCardToDOM con el ID generado
-      addCardToDOM(name, id, priority, year, description);
-    } catch (error) {
-      console.error(error);
-      mostrarModal("Ocurrió un error al guardar la semana en el servidor.");
-      return;
-    }
-  }
-
-  function addCardToDOM(name, id, priority, year, description) {
+  //Creacion de tarjetas
+  function createCard(name, id, week, month, year, description) {
     const cardContainer = document.createElement("div");
     cardContainer.classList.add("col-md-4", "mb-4");
-
+  
     const card = `
       <div class="card shadow-sm card-square" data-id="${id}">
         <div class="card-body">
           <h5 class="card-title"><b>${name}</b></h5>
           <p class ="card-text">Semana: ${week}</p>
-          <p class ="card-text">Prioridad: ${priority}</p>
+          <p class ="card-text">Mes: ${month}</p>
           <p class ="card-text">Año: ${year}</p>
           <p class ="card-text">Descripcion: ${description}</p>
         </div>
@@ -74,12 +40,42 @@ document.addEventListener("DOMContentLoaded", () => {
         </div>
       </div>
     `;
-
+  
     cardContainer.innerHTML = card;
-
+  
     const mainRow = document.querySelector("main .row");
     mainRow.appendChild(cardContainer);
+  
+    // Escuchar eventos de click en los iconos de eliminar
+    const deleteIcons = document.querySelectorAll(".bi-trash");
+    deleteIcons.forEach((icon) => {
+    icon.addEventListener("click", (event) => {
+    const card = event.target.closest(".card");
+    const cardId = card.getAttribute("data-id");
+    const deleteCardBtn = document.getElementById("eliminarTarjetaBtn");
+    deleteCardBtn.setAttribute("data-card", cardId);
+  });
+});
+
+// Escuchar evento de click en el botón de eliminar tarjeta
+const deleteCardBtn = document.querySelector("#eliminarTarjetaBtn");
+deleteCardBtn.addEventListener("click", () => {
+  const cardId = deleteCardBtn.getAttribute("data-card");
+  const card = document.querySelector(`[data-id="${cardId}"]`);
+  if (card) {
+    card.closest('.col-md-4.mb-4').remove();
+    const eliminarTarjetaModalEl = document.getElementById("eliminarTarjetaModal");
+    const eliminarTarjetaModal = bootstrap.Modal.getInstance(eliminarTarjetaModalEl);
+    eliminarTarjetaModal.hide();
   }
+});
+    
+  }
+ // La función deleteCard elimina una tarjeta del DOM.
+  function deleteCard(cardContainer) {
+    cardContainer.remove();
+  }
+  
 
   function mostrarModal(mensaje) {
     const modal = new bootstrap.Modal(document.getElementById("genericModal"));
@@ -91,31 +87,55 @@ document.addEventListener("DOMContentLoaded", () => {
   confirmBtn.addEventListener("click", async (e) => {
     var formulario = document.getElementById("cardForm");
     var inputsRequeridos = formulario.querySelectorAll("[required]");
+  
     var valido = true;
-
+  
     for (var i = 0; i < inputsRequeridos.length; i++) {
       if (!inputsRequeridos[i].value) {
         valido = false;
         break;
       }
     }
-
+  
     if (valido) {
       e.preventDefault();
       let name = document.getElementById("name").value;
       let week = document.getElementById("week").value;
-      let priority = document.getElementById("priority").value;
+      let month = document.getElementById("month").value;
       let year = document.getElementById("year").value;
       let description = document.getElementById("description").value;
 
-      let prioritySelect = document.getElementById("priority");
-      let priorityText = prioritySelect.options[prioritySelect.selectedIndex].text;
+      // validar  Nombre
+      const nameRegex = /^[a-zA-ZáéíóúñÁÉÍÓÚÑ\s]+$/;
+      if (!nameRegex.test(name)) {
+        mostrarModal("Por favor ingrese un nombre válido.");
+        return;
+      }
 
-      // Aquí se validan los campos del formulario
-      // ...
+      // validar  Semana
+      const weekRegex = /^(0?[1-9]|[1-4][0-9]|5[0-3])$/;
+      if (!weekRegex.test(week)) {
+      mostrarModal("Por favor ingrese un número de semana válido (entre 01 y 53).");
+      return;
+      }
 
-      // Llama a la función createCard con los datos del formulario
-      await createCard(name, week, priority, year, description);
+      // validar  campo de mes
+      const monthRegex = /^(0?[1-9]|1[0-2])$/;
+      if (!monthRegex.test(month)) {
+        mostrarModal("Por favor ingrese un mes válido (entre 1 y 12).");
+        return;
+      }
+
+      // validar  campo de año
+      const yearRegex = /^\d{4}$/;
+      if (!yearRegex.test(year)) {
+        mostrarModal("Por favor ingrese un año válido (formato: AAAA).");
+        return;
+      }
+
+      const id = generateRandomId();
+
+      createCard(name, id, week, month, year);
 
       // Cerrar el modal
       const nuevaSemanaModal = document.getElementById("nuevaSemanaModal");
