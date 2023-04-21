c55bc114bff78b3b5a882686ece53ed6925c979f
LauRoConUOC <lrodriguezconta@uoc.edu>
2023-04-21 13:09:43 +0200

    ELIMINACION DE SEMANAS COMPLETADA

--- Producto2/public/scripts/delete-week.js
+++ Producto2/public/scripts/delete-week.js
@@ -1,30 +1,63 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let selectedCard = null;
+document.addEventListener("DOMContentLoaded", async () => {
+  async function deleteWeekFromServer(id) {
+    try {
+      const response = await fetch(`/weeks/${id}`, { method: 'DELETE' });
 
-  // Escuchar eventos de click en los iconos de eliminar
-  const deleteIcons = document.querySelectorAll(".bi-trash");
-  deleteIcons.forEach((icon) => {
-    icon.addEventListener("click", (event) => {
-      const card = event.target.closest(".card");
-      selectedCard = card;
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log('Week eliminada:', result);
+
+    } catch (error) {
+      console.error("Error al eliminar la semana:", error);
+    }
+  }
+
+  async function deleteCard(id) {
+    const cardContainers = document.querySelectorAll(".col-md-4.mb-4");
+
+    cardContainers.forEach((cardContainer) => {
+      const card = cardContainer.querySelector(".card");
+
+      if (card.getAttribute("data-id") === id) {
+        cardContainer.remove();
+      }
     });
-  });
+  }
 
-  // Escuchar evento de click en el botón de eliminar tarjeta
-  const deleteCardBtn = document.querySelector("#eliminarTarjetaBtn");
-  deleteCardBtn.addEventListener("click", () => {
-    if (selectedCard) {
-      selectedCard.closest('.col-md-4.mb-4').remove();
-      selectedCard = null;
+  const mainRow = document.querySelector("main .row");
+
+  mainRow.addEventListener("click", (e) => {
+    if (e.target.classList.contains("delete-icon")) {
+      e.preventDefault();
+      const card = e.target.closest(".card");
+      const weekId = card.getAttribute("data-id");
+
       const eliminarTarjetaModalEl = document.getElementById("eliminarTarjetaModal");
-      const eliminarTarjetaModal = bootstrap.Modal.getInstance(eliminarTarjetaModalEl);
-      eliminarTarjetaModal.hide();
+      const eliminarTarjetaModal = new bootstrap.Modal(eliminarTarjetaModalEl, { keyboard: false });
+
+      const eliminarTarjetaBtn = eliminarTarjetaModalEl.querySelector("#eliminarTarjetaBtn");
+
+      // Elimina el evento click existente para evitar múltiples clics
+      eliminarTarjetaBtn.removeEventListener('click', handleClick);
+
+      // Agrega un nuevo evento click
+      eliminarTarjetaBtn.addEventListener("click", handleClick);
+
+      async function handleClick() {
+        await deleteWeekFromServer(weekId);
+        deleteCard(weekId);
+
+        const backdrop = document.querySelector('.modal-backdrop');
+        if (backdrop) {
+          backdrop.remove();
+        }
+        eliminarTarjetaModal.hide();
+      }
+
+      eliminarTarjetaModal.show();
     }
   });
 });
-
-
-
-
-
-
